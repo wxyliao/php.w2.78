4a2958e454d16959ef1743a0c421bb1116c282b6
wxyliao <pp84111666@gmail.com>
2021-03-16 17:17:12 +0800

    add css settings

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <!DOCTYPE html>
 <html>
-<title>wxy_web</title>
+<head>
+	
+    <title>wxy_web</title>
+    <style>
+       body {
+       background-color: lightblue;
+       }
+
+       h1 {
+       color: white;
+       text-align: center;
+       }
+
+       p {
+       font-family: verdana;
+       font-size: 20px;
+       }	
+    </style>
+</head>
 <body>
 
 <h1>This is a heading</h1>
