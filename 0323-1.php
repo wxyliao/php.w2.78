dc76e0dd7609f1e07ea61703a50dbed4fbca1e54
wxyliao <pp84111666@gmail.com>
2021-03-23 17:26:52 +0800

    W2 0323 change

---
+++ 0323-1.php
@@ -0,0 +1,9 @@
+<?php
+  $chi = 90;
+  $eng = 95;
+  $mat = 100;
+  $his = 85;
+  $total = $chi + $eng + $mat + $his;
+  $avg = $total / 4;
+  echo "Total is $total, Average is $avg";
+?>
--- index.php
+++ index.php
@@ -13,6 +13,11 @@
        text-align: center;
        }
 
+        h3 {
+       color: white;
+       text-align: center;
+       }
+
        p {
        font-family: verdana;
        font-size: 20px;
@@ -21,8 +26,22 @@
 </head>
 <body>
 
-<h1>This is a heading</h1>
-<p>This is a paragraph.</p>
+<h1>Welcome.</h1>
+<h3>This web is my homework<h3>
+
+<button><a href='http://localhost/wxy/'>HOME</a></button>
+<button><a href='https://id.nkust.edu.tw/'>Major</a></button>
+<button><a href='https://www.design-thinking.tw/'>苗圃計畫</a></button>
+
+<select onchange='location.href="/wxy_web/"+this.value'>
+  <option value="0323-1.php">請選擇要執行的練習</option>
+  <option value="0323-1.php">0323-1</option>
+  <option value="0323-2.php">0323-2</option>
+</select>
+
+<hr>
+
+<p>If you have any problem, that is my email : c109101136@nkust.edu.tw</p>
 
 </body>
 </html>
